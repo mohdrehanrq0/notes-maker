4248bb88c898dd12f1f4ea357e89e16a86e3f145
Mohammad Rehan Qureshi <53863785+mohdrehanrq0@users.noreply.github.com>
2024-02-28 22:29:20 +0530

    changes

--- lib/youtube.ts
+++ lib/youtube.ts
@@ -1,9 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const searchYoutube = async (searchString: string) => {
   const searchQuery = encodeURIComponent(searchString);
   const { data } = await axios.get(
-    `https://www.googleapis.com/youtube/v3/search?key=AIzaSyDwB0aMsuiy1_kD_luOfhdKf2TKBJF-qE8&q=${searchQuery}&videoDuration=medium&videoEmbeddable=true&type=video&maxResults=2`
+    `https://www.googleapis.com/youtube/v3/search?key=${process.env.YOUTUBE_API_KEY}&q=${searchQuery}&videoDuration=medium&videoEmbeddable=true&type=video&maxResults=2`
   );
   if (!data) {
     console.log("youtube fail");
